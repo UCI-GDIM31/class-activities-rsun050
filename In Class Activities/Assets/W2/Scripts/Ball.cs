ea777a936be4150a74fe3f72b564325b217475a9
Ruth Sun <rsun050@ucr.edu>
2025-10-06 18:06:24 -0700

    steps 1-9, update README

--- In Class Activities/Assets/W2/Scripts/Ball.cs
+++ In Class Activities/Assets/W2/Scripts/Ball.cs
@@ -16,79 +16,85 @@ public class Ball : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // STEP 1 -------------------------------------------------------------
-        // Uncomment ONE of the blow lines of code to ADD ONE
-        //      to the value of the variable named '_bounces'.
-        //_bounces = _bounces + 2;
-        //_bounces + 1
-        //_bounces --;
-        //_bounces++;
-        //_bounces += 1
+        // Uncomment ONE of the below lines of code to ADD ONE
+        // to the value of the variable named '_bounces'.
+        //_bounces = _bounces + 2;      // adds 2
+        //_bounces + 1                  // bad syntax
+        //_bounces --;                  // subtracts 1
+        _bounces++;                     // LGTM
+        //_bounces += 1                 // also works
         // STEP 1 -------------------------------------------------------------
 
         // This line sets the value of the text on screen to the word "Bounces"
-        //      plus the number of times the ball has bounced.
+        // plus the number of times the ball has bounced.
         // We can use the addition operator on strings too!
         // It concatenates (combines) the strings together into one long string.
-        _bouncesText.text = "Bounces: " + _bounces;
+        _bouncesText.text = "Bounces: " + _bounces; // joyous!
 
         // In data, colors are usually stored as "RGB channels", meaning,
-        //      Red, Green, and Blue are mixed together in different intensities
-        //      to create the color.
+        // Red, Green, and Blue are mixed together in different intensities
+        // to create the color.
         // These variables represent the Red, Green, and Blue values
-        //      of the color of your ball.
+        // of the color of your ball.
         float r = _spriteRenderer.color.r;
         float g = _spriteRenderer.color.g;
         float b = _spriteRenderer.color.b;
 
         // STEP 2 -------------------------------------------------------------
         // Uncomment ONE of the below lines to ADD 0.1 to the value of 'r'.
-        //r += 0.1f
-        //r * 0.1;
-        //r = r + 0.1f;
-        //r = r * 0.1f;
-        //r += 0.1;
+        // r += 0.1f                // missing semicolon :(            this one got me for a sec >:(
+        // r * 0.1;                 // missing f for float + bad syntax
+        r = r + 0.1f;               // LGTM
+        //r = r * 0.1f;             // sets r to 10% of its original value
+        // r += 0.1;                // missing f for float             this one also got me
         // STEP 2 -------------------------------------------------------------
 
         // RGB values can only go from 0.0 - 1.0.
         // So, if the value of 'r' gets to above 1.0, we want to set it back to 0.0
-        //      so that we can start over and keep increasing it again.
+        // so that we can start over and keep increasing it again.
         if (r > 1.0f)
         {
             // STEP 3 ---------------------------------------------------------
             // Uncomment ONE of the below lines of code to set the value of 'r' to ZERO.
-            //r = 0.0f
-            //r = 0.0f;
-            //r 0.0f;
+            //r = 0.0f              // missing semicolon
+            r = 0.0f;               // LGTM
+            //r 0.0f;               // where operator?
             // STEP 3 ---------------------------------------------------------
         }
 
         // STEP 4 -------------------------------------------------------------
         // Uncomment the below line and fix it to SUBTRACT 0.1 from the value of 'g'.
-        //g -= 0.1f
+        //g -= 0.1f                 // missing semicolon lol
+        g -= 0.1f;
         // STEP 4 -------------------------------------------------------------
 
         if (g < 0.0f)
         {
             // STEP 5 ---------------------------------------------------------
             // Uncomment the below line and fix it to set the value of 'g' to ONE.
-            //g = 1.0;
+            //g = 1.0;              // missing f for float
+            g = 1.0f;
             // STEP 5 ---------------------------------------------------------
         }
 
         // STEP 6 -------------------------------------------------------------
         // Below this comment, write a line of code to MULTIPLY 'b' by 1.2.
+        b *= 1.2f;
 
         // STEP 6 -------------------------------------------------------------
 
         // STEP 7 -------------------------------------------------------------
         // Below this comment, write an IF STATEMENT:
         // IF the value of 'b' is GREATER THAN **OR** EQUAL TO 1.0,
-        //      set 'b' to 0.1. 
-        
+        // set 'b' to 0.1. 
+        if (b >= 1.0f)
+        {
+            b = 0.1f;
+        }
         // STEP 7 -------------------------------------------------------------
 
         // This line assigns a new color to your Ball with the new RGB values
-        //      we just created.
+        // we just created.
         // Don't worry about how it works yet; we'll learn about this later :)
         Color newColor = new Color(r, g, b);
         _spriteRenderer.color = newColor;
@@ -99,16 +105,17 @@ public class Ball : MonoBehaviour
 
         // STEP 8 -------------------------------------------------------------
         // A simple way to calculate how bright a color will be is by
-        //      averaging the R, G, B values.
+        // averaging the R, G, B values.
         // Below this comment, write 2 lines of code to ADD the values of 
-        //      r, g, and b, and then divide the result by 3.
+        // r, g, and b, and then divide the result by 3.
         // Store the result in a variable named 'brightness'.
+        float brightness = (r + g + b) / 3f;
         
         // STEP 8 -------------------------------------------------------------
 
         // STEP 9 -------------------------------------------------------------
         // Uncomment the below line to make the 'brightness' text change in your game.
-        //_brightnessText.text = "brightness = " + brightness;
+        _brightnessText.text = "brightness = " + brightness;
         // STEP 9 -------------------------------------------------------------
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # in-class-activities
 ## Devlogs
 ### W1
-<a style="font-size:70%;"><i>hello world</i></a>
-
-Write your W1 activity Devlog here. <a style="font-size:70%"><- ok</a>
-
-<b style="font-size:120%">Prompt: In the Hierarchy, move the Camera off of the Cat GameObject, so that it’s no longer a child of the Cat. What happens when you run the game now, and why?</b>
+<b>In the Hierarchy, move the Camera off of the Cat GameObject, so that it’s no longer a child of the Cat. What happens when you run the game now, and why?</b>
 
 The cat moves, but the camera stays still. Since the camera is no longer a 'child object' of the cat, so their positions are not connected.
 <hr>
 
 ### W2
-Create future Devlog sub-headers with the three # symbols, then write your Devlogs below them.
+<b>1. Why are the `r`, `g`, and `b` variables `floats` instead of `ints`, `bools`, or `strings`?</b>
+	a. `bool` and `string` don't really make sense. Maybe `int` would make sense, but the SpriteRenderer saves its RGB values as `floats`. Either way, RGB represents 'how red' or 'how green' or 'how blue', so we need a variable type that can represent a scale, which `bool` and `string` can't do.
+	b. `bool` could work if you want it to be "YEAH IT'S RED" or "NO IT'S NOT RED"... I think that gives you like 8 possible color combinations.
+<br><b>2. Why is the _bounce variable an int instead of a float, bool, or string?</b>
+	a. Not `float` because can't have 0.5 of a bounce, although you could have `_bounces + 1.0f`? Not `bool` because we need to keep track of how *many* bounces. Not `string` because text isn't numbers, and you can't do numbers math with text. `int` makes the most sense.
+<br><b>3. The error you got after Step X of Part 2 told you something useful about why that line of code was broken- what was it?</b>
+	a. A lot of times for the "uncomment the right line of code" steps I would forget that I need an f for `float` variables, and I would get an error for some variable type called `double`.
+
+<hr>
+
+### W3
+<hr>
 
 ## Open-Source Assets
 ### W1
